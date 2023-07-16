1453f7a2664a1288a7208ef21c8ee95f56062a52
Ellahjonnez <Ellahjonnez@gmail.com>
2023-07-16 08:30:56 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -21,11 +21,11 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+            for keys, values in kwargs.items():
+                if keys == "created_at" or keys == "updated_at":
+                    self.__dict__[keys] = datetime.strptime(n, tform)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[keys] = values
         else:
             models.storage.new(self)
 
